8ab020823872e03993d673cbce449969326e75b3
vathana Tork <vathana@cam-mob.com>
2024-06-03 13:45:18 +0700

    custom template html

--- app/Http/Resources/Mobile/UniversityOverviewResource.php
+++ app/Http/Resources/Mobile/UniversityOverviewResource.php
@@ -5,14 +5,18 @@ namespace App\Http\Resources\Mobile;
 use App\Http\Resources\ContactResource;
 use App\Http\Resources\DegreeLevelListResource;
 use App\Http\Resources\UniversityTypeListResource;
+use App\Http\Traits\GenerateHtmlTrait;
 use Illuminate\Http\Request;
 
 /**
  * @property mixed $degreeLevels
  * @property mixed $contact
+ * @property mixed $description
+ * @method generateHtmlUniversityDescription(mixed $description)
  */
 class UniversityOverviewResource extends UniversityListResource
 {
+    use GenerateHtmlTrait;
     /**
      * Transform the resource into an array.
      *
@@ -27,7 +31,8 @@ class UniversityOverviewResource extends UniversityListResource
         unset($data['type_en'],$data['type_kh'],$data['logo_image'], $data['image']);
 
         return $data + [
-                'description' => $this->description ?? null,
+                'description' => $this->description ? $this->generateHtmlUniversityDescription($this->description) :
+                    null,
                 'type' => $this->type ? UniversityTypeListResource::make($this->type) : null,
                 'study_option' => $this->degreeLevels ? DegreeLevelListResource::collection($this->degreeLevels) : null,
                 'contact' => $this->contact ? ContactResource::make($this->contact) : null
---
+++ app/Http/Traits/GenerateHtmlTrait.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Traits;
+
+use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
+
+trait  GenerateHtmlTrait
+{
+    public function generateHtmlUniversityDescription(string $content): array|bool|string
+    {
+        $html = @file_get_contents(storage_path() . '/html/template-description-university.html');
+        $html = str_replace('<!--CONTENT-->', $content, $html);
+
+        if (!empty($thumbnail)){
+            $stringImage = '<img src="' . asset($thumbnail) . '" alt="News thumbnail">';
+            $html = str_replace('<!--IMAGE THUMBNAIL-->', $stringImage, $html);
+        }
+
+        if (!empty($content)) {
+            $html = str_replace('<!--CONTENT-->', $content, $html);
+        }
+
+        // Instantiate the CssToInlineStyles library
+        $cssToInlineStyles = new CssToInlineStyles();
+        $htmlWithInlineStyles = $cssToInlineStyles->convert($html);
+
+        // Optionally, replace double quotes and new lines
+        $htmlWithInlineStyles = $this->__replaceDoubleQuote($htmlWithInlineStyles);
+        return $this->__replaceSlashN($htmlWithInlineStyles);
+    }
+
+
+    public function generateHtmlArticle($title, $thumbnail, $content): array|bool|string
+    {
+        $html = @file_get_contents(storage_path() . '/html/template-news.html');
+        $html = str_replace('<!--TITLE-->', $title, $html);
+
+        if (!empty($thumbnail)){
+            $stringImage = '<img src="' . asset($thumbnail) . '" alt="News thumbnail">';
+            $html = str_replace('<!--IMAGE THUMBNAIL-->', $stringImage, $html);
+        }
+
+        if (!empty($content)) {
+            $html = str_replace('<!--CONTENT-->', $content, $html);
+        }
+        $html = $this->__replaceDoubleQuote($html);
+        return  $this->__replaceSlashN($html);
+    }
+
+    public function generateHtmlInternationalAffair($title, $thumbnail, $content): array|bool|string
+    {
+        $html = @file_get_contents(storage_path() . '/html/template-internationalAffair.html');
+        $html = str_replace('<!--TITLE-->', $title, $html);
+
+        if (!empty($thumbnail)){
+            $stringImage = '<img src="' . asset($thumbnail) . '" alt="News thumbnail">';
+            $html = str_replace('<!--IMAGE THUMBNAIL-->', $stringImage, $html);
+        }
+
+        if (!empty($content)) {
+            $html = str_replace('<!--CONTENT-->', $content, $html);
+        }
+
+        $html = $this->__replaceDoubleQuote($html);
+        return  $this->__replaceSlashN($html);
+    }
+
+    public function generateHtmlSecretariat($title, $thumbnail, $content): array|bool|string
+    {
+        $html = @file_get_contents(storage_path() . '/html/template-secretariat.html');
+        $html = str_replace('<!--TITLE-->', $title, $html);
+
+        if (!empty($thumbnail)){
+            $stringImage = '<img src="' . asset($thumbnail) . '" alt="News thumbnail">';
+            $html = str_replace('<!--IMAGE THUMBNAIL-->', $stringImage, $html);
+        }
+
+        if (!empty($content)) {
+            $html = str_replace('<!--CONTENT-->', $content, $html);
+        }
+
+        $html = $this->__replaceDoubleQuote($html);
+        return  $this->__replaceSlashN($html);
+    }
+    private function __replaceDoubleQuote(string $string): string
+    {
+        return str_replace('"', "'", $string);
+    }
+    private function __replaceSlashN(string $string): string
+    {
+        return str_replace("\n", " ", $string);
+    }
+
+    private function __replaceSlashR(string $string): string
+    {
+        return str_replace("\r", " ", $string);
+    }
+}
