6132c7b86a48152096581791a738dc9777d6b534
vathana Tork <vathana@cam-mob.com>
2024-06-04 15:34:59 +0700

    update major name resource

--- app/Http/Resources/Admin/MajorNameResource.php
+++ app/Http/Resources/Admin/MajorNameResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Admin;
 
+use App\Helpers\UrlHelper;
 use App\Http\Resources\MajorListResource;
 use Illuminate\Http\Request;
 
@@ -21,7 +22,8 @@ class MajorNameResource extends MajorListResource
         $data = parent::toArray($request);
 
         return $data + [
-            'image_url' => $this->image_url,
+            'image_url' => $this->image_url ? UrlHelper::resolveUrl($this->image_url,env('MINIO_BASE_URL',
+                    null)) : null,
             'is_active' => $this->is_active
         ];
     }
