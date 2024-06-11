02221d61dfa7e69288db325c6a9ad82087a16331
vathana Tork <vathana@cam-mob.com>
2024-06-11 11:36:38 +0700

    mobile program

---
+++ app/Http/Resources/Mobile/UniversityProgramResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Resources\Mobile;
+
+use App\Helpers\UrlHelper;
+use App\Http\Resources\DegreeLevelListResource;
+use App\Http\Resources\MajorListResource;
+use App\Models\MajorAndSpecializeName;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property mixed $majors
+ * @property mixed $degreeLevels
+ */
+class UniversityProgramResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array
+    {
+        $specializes = $this->majors ? $this->majors->pluck('specialize')->flatten()->filter() : collect();
+
+        return [
+            'degreeLevels' => DegreeLevelListResource::collection($this->whenLoaded('degreeLevels')),
+            'majors' => MajorResource::collection($this->whenLoaded('majors')),
+            'specialize' => SpecializeResource::collection($specializes),
+        ];
+    }
+}
+
+
+class MajorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'is_active' => $this->is_active,
+            'major_name' => $this->when(
+                $this->relationLoaded('majorName') && $this->majorName,
+                MajorListResource::make($this->majorName)
+            ),
+            'major_image' => $this->when(
+                $this->relationLoaded('majorName') && $this->majorName && $this->majorName->image_url,
+                UrlHelper::resolveUrl($this->majorName->image_url, env('MINIO_BASE_URL', null))
+            )
+        ];
+    }
+}
+
+
+
+class SpecializeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'is_active' => $this->is_active,
+            'specialize_name' => $this->when(
+                $this->relationLoaded('specializeName') && $this->specializeName,
+                MajorListResource::make($this->specializeName)
+            ),
+            'specialize_image' => $this->when(
+                $this->relationLoaded('specializeName') && $this->specializeName && $this->specializeName->image_url,
+                UrlHelper::resolveUrl($this->specializeName->image_url, env('MINIO_BASE_URL', null))
+            ),
+        ];
+    }
+}
+
+
+
+
--- app/Models/Specialize.php
+++ app/Models/Specialize.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Specialize extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    public function specializeName(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(MajorAndSpecializeName::class,'major_name_id');
+    }
 }
