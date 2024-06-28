3e599c650000bbd07ab1d385955bc8bf0e55010f
vathana Tork <vathana@cam-mob.com>
2024-06-28 15:54:35 +0700

    scholarship mobile

--- app/Models/Scholarship.php
+++ app/Models/Scholarship.php
@@ -12,6 +12,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @method static latest()
  * @method static where(string $string, string $id)
  * @method static count()
+ * @method static isActive(int $int)
  */
 class Scholarship extends Model
 {
@@ -30,6 +31,15 @@ class Scholarship extends Model
     public function scopeIsActive($query,$param)
     {
         return $query->where('is_active',$param);
+    }
+
+    public function scopeSearch($query, $params)
+    {
+        // Convert the search term to lowercase
+        $search = strtolower($params);
+        // Perform the query with case-insensitive search on name_en and name_kh fields
+        return $query->whereRaw('LOWER(name_en) COLLATE utf8mb4_general_ci LIKE ?', ['%' . $search . '%'])
+            ->orWhereRaw('LOWER(name_kh) COLLATE utf8mb4_general_ci LIKE ?', ['%' . $search . '%']);
     }
 
 }
